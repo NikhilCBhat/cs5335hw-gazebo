14b2bffd263251be11c2a4cd2220523c05905a91
nikhilcbhat <bhatnikhil314@gmail.com>
2020-09-12 15:35:08 -0400

    Got a solution working

--- nikhil-hw01/brain.cc
+++ nikhil-hw01/brain.cc
@@ -11,14 +11,27 @@ const double goal_x = 20.0;
 const double goal_y = 0.0;
 bool done = false;
 
+// Tweakable Parameters
+const double normal_vel = 3;
+
+const double obstacle_avoidance_vel = 2;
+const double obstacle_avoidance_turn = 0.8;
+
+const double obstacle_detection_range = 2.5;
+const double obstacle_detection_angle = 0.8;
+
 void
 callback(Robot* robot)
 {
-    /*
+
+    double angle_to_goal = atan2(goal_y-robot->pos_y, goal_x-robot->pos_x);
+    double turn_angle = robot->pos_t - angle_to_goal;
+
+    
     cout << endl;
     cout << "robot x =" << robot->pos_x << endl;
     cout << "robot y =" << robot->pos_y << endl;
-    */
+    
     double dx = goal_x - robot->pos_x;
     double dy = goal_y - robot->pos_y;
     if (abs(dx) < 0.75 && abs(dy) < 0.75) {
@@ -32,20 +45,21 @@ callback(Robot* robot)
     bool turn = false;
 
     for (LaserHit hit : robot->hits) {
-        if (hit.range < 1.5) {
-            if (hit.angle < 0.5 || hit.angle > (6.2 - 0.5)) {
+        if (hit.range < obstacle_detection_range) {
+            if (abs(hit.angle) < obstacle_detection_angle) {
                 turn = true;
             }
         }
     }
 
     if (turn) {
-        robot->set_vel(3.0);
-        robot->set_turn(0.5);
+        cout << "Obstacle detected!" << endl;
+        robot->set_vel(obstacle_avoidance_vel);
+        robot->set_turn(obstacle_avoidance_turn);
     }
     else {
-        robot->set_vel(5.0);
-        robot->set_turn(-0.02);
+        robot->set_vel(normal_vel);
+        robot->set_turn(turn_angle);
     }
 }
 
